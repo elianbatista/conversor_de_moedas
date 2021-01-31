b2446b7a93cded280a7accc83677a4a98198103a
Elian Batista <elianbatista2000@gmail.com>
2021-01-30 21:49:04 -0300

    Trabalhando com Futuro

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=07f16c46";
 
-void main() async {
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)['results']['currencies']['USD']);
-
+void main() {
   runApp(MaterialApp(
-    home: Container()
+    home: Home()
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Conversor de Moedas'),
+        backgroundColor: Colors.amber,
+        centerTitle: true
+      )
+    );
+  }
 }
