454805698887876ac2cb269d6441368a8a837266
Elian Batista <elianbatista2000@gmail.com>
2021-01-30 21:42:07 -0300

    Requisitando os Dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
 
-void main() {
-  
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=07f16c46";
+
+void main() async {
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)['results']['currencies']['USD']);
+
+  runApp(MaterialApp(
+    home: Container()
+  ));
 }
